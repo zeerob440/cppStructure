f5d57e5a749f88e7bde7311d1cdb8e126759cc20
MU-TH-ER <zeerob440@tutanota.com>
2025-12-31 13:44:45 -0500

    Docs: added cppStructure to gitHub repo

---
+++ cppStructure.cpp
@@ -0,0 +1,27 @@
+/* 
+This program explains the basic structure of C++ programs in general.
+It is a very small program, but an important milestone in learning C++.
+Proudly Engineered by Zachary Roberts, 31 DEC 2025.
+"INTERFACE 2037 READY FOR INQUIRY" MU-TH-ER Alien, 1979
+*/
+/*PREPROCESSOR DIRECTIVE: this includes the source code for declarations before compiling.
+preprocessors always start with octothorpe<filename> such as #<iostream>
+*/ 
+#include <iostream>
+
+//Beginning of program: This is the function block main(), int is INTEGER, it serves as the return value of main().
+int main() 
+{ //Opening main() curly brace.
+    /*output this to screen std::cout << "Hello 46.127.53.128" << std::endl;
+
+    std is STANDARD LIBARY.
+    :: is a SCOPE RESOLUTION OPERATOR, it connects things.
+    << is INSERTION OPERATOR, it inserts data into the stream
+    cout is CHARACTER OUTPUT. 
+    endl; end(L, NOT "one", 1 != l). Creates new line, flushes output stream.
+    */
+    std::cout << "Hello, 46.127.53.238." << std::endl;
+
+    //RETURN Ends the program and exits. Programs end with return 0;
+    return 0;
+} // closing main() brace
